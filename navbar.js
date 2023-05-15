51458dcb576119f838abdcf212536e4ce4d811a5
Temirlan <temirlan.kystaubayev@gmail.com>
2023-05-15 13:50:42 +0600

    feat: add toggle animation in 'resposive navbar'

--- navbar.js
+++ navbar.js
@@ -3,4 +3,5 @@ const toggle = document.querySelector(".nav-toggle");
 toggle.addEventListener("click", function() {
     const links = document.querySelector(".links");
     links.classList.toggle("show-links");
+    toggle.classList.toggle("nav-toggle-anim");
 });
